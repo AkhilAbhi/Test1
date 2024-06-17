438d9dbcdcd65346a3b214e521d1a978b7ec2b89
AkhilAbhi <66011589+AkhilAbhi@users.noreply.github.com>
2024-06-17 12:44:21 +0530

    Update st.py

--- st.py
+++ st.py
@@ -3,9 +3,7 @@ from pyrogram import Client, filters
 # Replace 'my_bot' with a unique name for your bot session and provide your bot token, api_id, and api_hash.
 app = Client(
     "my_bot",
-    api_id=27376757,
-    api_hash="27d4e363b3524401b62e86f1cc16c096",
-    bot_token="7413073877:AAFIMuTZM0IQjANvmakArpS0sfUuOU7ohj4"
+
 )
 
 # Handler for start command
